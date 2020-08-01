88d68a349adab21b5bc83e130abcccb2a25e01a3
tejasjokin <61027009+tejasjokin@users.noreply.github.com>
2020-08-01 20:33:12 +0530

    Updates

---
+++ precache-manifest.cb50f055216a39151c23d8eda545c420.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef2657b1b06ade39d63768e981b1be8",
+    "url": "/sillyapi/index.html"
+  },
+  {
+    "revision": "59f4d961c6c424e44317",
+    "url": "/sillyapi/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "60f412b89dc24287651f",
+    "url": "/sillyapi/static/css/main.dd1fd9ca.chunk.css"
+  },
+  {
+    "revision": "59f4d961c6c424e44317",
+    "url": "/sillyapi/static/js/2.a761a3e4.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/sillyapi/static/js/2.a761a3e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f412b89dc24287651f",
+    "url": "/sillyapi/static/js/main.55990b20.chunk.js"
+  },
+  {
+    "revision": "b537e90a1ade461dfc9f",
+    "url": "/sillyapi/static/js/runtime-main.5f06cb92.js"
+  },
+  {
+    "revision": "0cc4e978640f1825484c8537e41209f3",
+    "url": "/sillyapi/static/media/backslash-rpjol-webfont.0cc4e978.woff"
+  },
+  {
+    "revision": "c7359c5c7d5a26fe7abb0024ca6cb504",
+    "url": "/sillyapi/static/media/backslash-rpjol-webfont.c7359c5c.woff2"
+  },
+  {
+    "revision": "2dacdc3b7313a07e7f2c9ed1dae4035c",
+    "url": "/sillyapi/static/media/typewcond_regular-webfont.2dacdc3b.woff2"
+  },
+  {
+    "revision": "3b19eac0030e3504e41e320a0a74a5f9",
+    "url": "/sillyapi/static/media/typewcond_regular-webfont.3b19eac0.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/puntastic/precache-manifest.d9e882b3664330c096966214faa614fd.js"
+  "/sillyapi/precache-manifest.cb50f055216a39151c23d8eda545c420.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puntastic/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sillyapi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
